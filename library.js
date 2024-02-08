bb6576f15e0c1fee1e43027d446b621cb6a13615
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-08 11:12:56 -0500

    Add displayLibrary function

--- library.js
+++ library.js
@@ -6,5 +6,23 @@ function Book(title) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    return;
 }
+
+function displayLibrary() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => {
+        container.removeChild(card);
+    });
+
+    let card = document.createElement('div');
+    card.setAttribute('class', 'card');
+
+    myLibrary.forEach((book) => {
+        card.textContent = book.title;
+        container.appendChild(card);
+    });
+}
+
+const container = document.querySelector('.container');
+
+displayLibrary();
